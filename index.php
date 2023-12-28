e2133c64f264fa29a03e3c17bbf82d818228487b
yamenTaleb <yamentaleb34@gmail.com>
2023-12-28 15:34:52 +0300

    add two files

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+// function equality()
+// { 
+//     $num = readline("");
+//     $array = [];
+//     for ($i = 0; $i < $num; $i++) {
+//         fscanf(STDIN,"%d",$array[$i]);
+//     }
+//     $q = min($array);
+//     $count =0;
+//     for ($i = 0; $i < $num; $i++) {
+//         $a = $array[$i] - $q;
+//         $count +=$a;
+//     }
+//     echo $count;
+// }
+// function transString($b)
+// {
+//     $onion =0;
+//     $loaf=0;
+//     for($i =0 ;$i<strlen($b);$i++):
+//     if($b[$i]=='O'){
+//         $onion++;
+//     } else {
+//         $loaf++;
+//     }
+//     endfor;
+//     if(($b[0]=='O'&& $onion>2)||($b[0]=='L'&& $loaf>2)) {
+//         return 1;
+//     } else {
+//         return -1;
+//     }
+// }
+// $s =readline();
+// $a =(string) readline();
+// echo transString($a);
+
+function transString($b, $a)
+{
+for($i = 0 ;$i <strlen($b);$i++){
+    if($b[$i]<=$a):
+        array_sic
+        break;
+        endif;
+}
+}
+$s =readline();
+$
+$a =(string) readline();
+while($s--){
+    transString($a,$ss);
+}
